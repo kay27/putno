2e82627bd93d6462c9e27ef32da425a778607674
Konstantin Yeliseyev <kay27@bk.ru>
2019-03-10 05:12:05 +0400

    v1.1: better key code handler

--- putno.cxx
+++ putno.cxx
@@ -1,5 +1,5 @@
 // Putno Fixer by kay27
-// version 1 alpha for mingw-w64
+// version 1.1 for mingw-w64
 // mailto: kay27@bk.ru
 
 #define _WIN32_WINNT 0x0400
@@ -18,81 +18,79 @@ SHORT shift = 0;
 SHORT control = 0;
 SHORT alt = 0;
 
+inline bool FixLastWord()
+{
+    if(counter==0) return false;
+
+    fixing = 1;
+
+    INPUT inp;
+    inp.type=INPUT_KEYBOARD;
+    inp.ki.wScan = 0;
+    inp.ki.time = 0;
+    inp.ki.dwExtraInfo = 0;
+    inp.ki.wVk = 8;
+    for(auto i=0; i<counter; i++)
+    {
+      inp.ki.dwFlags = 0;
+      SendInput(1, &inp, sizeof(INPUT));
+      inp.ki.dwFlags = KEYEVENTF_KEYUP; 
+      SendInput(1, &inp, sizeof(INPUT));
+    }
+
+    inp.ki.dwFlags = 0;
+    inp.ki.wVk = 162;
+    SendInput(1, &inp, sizeof(INPUT));
+    inp.ki.wVk = 160;
+    SendInput(1, &inp, sizeof(INPUT));
+    inp.ki.dwFlags = KEYEVENTF_KEYUP; 
+    SendInput(1, &inp, sizeof(INPUT));
+    inp.ki.wVk = 162;
+    SendInput(1, &inp, sizeof(INPUT));
+
+    for(auto i=0; i<counter; i++)
+    {
+      inp.ki.wVk = buffer[i].vkCode;
+      inp.ki.wScan = buffer[i].scanCode;
+      inp.ki.dwFlags = 0;
+      SendInput(1, &inp, sizeof(INPUT));
+      inp.ki.dwFlags = KEYEVENTF_KEYUP; 
+      SendInput(1, &inp, sizeof(INPUT));
+    }
+
+    fixing = 0;
+
+    return true;
+}
+
 __declspec(dllexport) LRESULT CALLBACK KeyboardEvent (int nCode, WPARAM wParam, LPARAM lParam)
 {
     if((nCode != HC_ACTION) || ((wParam != WM_KEYDOWN)&&(wParam != WM_KEYUP)))
         return CallNextHookEx(hKeyboardHook, nCode,wParam,lParam);
 
     KBDLLHOOKSTRUCT hook = *((KBDLLHOOKSTRUCT*)lParam);
-    DWORD vkCode  = hook.vkCode;
+    DWORD vkCode = hook.vkCode;
 
     if((vkCode == VK_SHIFT  )||(vkCode == VK_LSHIFT)  ||(vkCode == VK_RSHIFT  )) shift   = (wParam == WM_KEYDOWN);
     if((vkCode == VK_CONTROL)||(vkCode == VK_LCONTROL)||(vkCode == VK_RCONTROL)) control = (wParam == WM_KEYDOWN);
     if((vkCode == VK_MENU   )||(vkCode == VK_LMENU   )||(vkCode == VK_RMENU   )) alt     = (wParam == WM_KEYDOWN);
 
-    if(                        (vkCode == VK_LWIN    )||(vkCode == VK_RWIN    ))
-    {
-        counter = 0;
-        return CallNextHookEx(hKeyboardHook, nCode,wParam,lParam);
-    }
-
-
     if(fixing) return 0;
 
-    if((vkCode == 0)||(wParam != WM_KEYDOWN)) return CallNextHookEx(hKeyboardHook, nCode,wParam,lParam);
+    if((vkCode == 0)||(wParam != WM_KEYDOWN))
+        return CallNextHookEx(hKeyboardHook, nCode,wParam,lParam);
 
-    bool do_putno_fix = (!(shift||control||alt)) && (vkCode == VK_PAUSE);
-
-    if (do_putno_fix)
+    if ((!(shift||control||alt)) && (vkCode == VK_PAUSE))
     {
-        if(counter==0) return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
-        fixing = 1;
-        INPUT inp;
-        inp.type=INPUT_KEYBOARD;
-        inp.ki.wScan = 0;
-        inp.ki.time = 0;
-        inp.ki.dwExtraInfo = 0;
-        inp.ki.wVk = 8;
-        for(auto i=0; i<counter; i++)
-        {
-          inp.ki.dwFlags = 0;
-          SendInput(1, &inp, sizeof(INPUT));
-          inp.ki.dwFlags = KEYEVENTF_KEYUP; 
-          SendInput(1, &inp, sizeof(INPUT));
-        }
-
-        inp.ki.dwFlags = 0;
-        inp.ki.wVk = 162;
-        SendInput(1, &inp, sizeof(INPUT));
-        inp.ki.wVk = 160;
-        SendInput(1, &inp, sizeof(INPUT));
-        inp.ki.dwFlags = KEYEVENTF_KEYUP; 
-        SendInput(1, &inp, sizeof(INPUT));
-        inp.ki.wVk = 162;
-        SendInput(1, &inp, sizeof(INPUT));
-
-        for(auto i=0; i<counter; i++)
-        {
-          inp.ki.wVk = buffer[i].vkCode;
-          inp.ki.wScan = buffer[i].scanCode;
-          inp.ki.dwFlags = 0;
-          SendInput(1, &inp, sizeof(INPUT));
-          inp.ki.dwFlags = KEYEVENTF_KEYUP; 
-          SendInput(1, &inp, sizeof(INPUT));
-        }
-
-        fixing = 0;
-        return 0;
+        if(FixLastWord()) return 0;
     }
 
-    bool reset_counter = (vkCode < 33) && (vkCode != VK_PAUSE);
-
-    if(reset_counter)
+    else if((vkCode == 8)||(vkCode == 37)) // backspace, arrow left
     {
-        buffer[counter++]=hook;
-        counter = 0;
+        if(counter>0) counter--;
     }
-    else
+
+    else if( ((vkCode >= 48)&&(vkCode <= 90)) || ((vkCode >= 96)&&(vkCode <= 111)) || ((vkCode >= 186)&&(vkCode <= 222)))
     {
         buffer[counter++]=hook;
         if(counter >= BUFFER_SIZE)
@@ -100,6 +98,11 @@ __declspec(dllexport) LRESULT CALLBACK KeyboardEvent (int nCode, WPARAM wParam, 
             counter = BUFFER_SIZE/2;
             memcpy(buffer, &buffer[BUFFER_SIZE/2], BUFFER_SIZE / 2 * sizeof(KBDLLHOOKSTRUCT));
         }
+    }
+
+    else
+    {
+        counter = 0;
     }
 
     return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
