606986277b37d01ac88a4c0a16767c57ba6ae262
Konstantin Yeliseyev <kay27@bk.ru>
2019-03-12 05:31:08 +0400

    1.2: hotkey detection

--- putno.cxx
+++ putno.cxx
@@ -1,5 +1,5 @@
 // Putno Fixer by kay27
-// version 1.1 for mingw-w64
+// version 1.2 for mingw-w64
 // mailto: kay27@bk.ru
 
 #define _WIN32_WINNT 0x0400
@@ -9,14 +9,13 @@
 #include <vector>
 #include <string>
 
-const int BUFFER_SIZE=1000;
+const int BUFFER_SIZE=500;
 HHOOK hKeyboardHook;
 KBDLLHOOKSTRUCT buffer[BUFFER_SIZE];
-int counter = 0;
-int fixing = 0;
-SHORT shift = 0;
-SHORT control = 0;
-SHORT alt = 0;
+int counter = 0, fixing = 0;
+SHORT shift = 0, control = 0, alt = 0;
+char hotkey;
+DWORD key1=164, key2=160;
 
 inline bool FixLastWord()
 {
@@ -39,13 +38,20 @@ inline bool FixLastWord()
     }
 
     inp.ki.dwFlags = 0;
-    inp.ki.wVk = 162;
+    inp.ki.wVk = key1;
     SendInput(1, &inp, sizeof(INPUT));
-    inp.ki.wVk = 160;
-    SendInput(1, &inp, sizeof(INPUT));
-    inp.ki.dwFlags = KEYEVENTF_KEYUP; 
-    SendInput(1, &inp, sizeof(INPUT));
-    inp.ki.wVk = 162;
+
+    if(key2)
+    {
+        inp.ki.wVk = key2;
+        SendInput(1, &inp, sizeof(INPUT));
+        inp.ki.dwFlags = KEYEVENTF_KEYUP; 
+        SendInput(1, &inp, sizeof(INPUT));
+        inp.ki.wVk = key1;
+    }
+    else
+        inp.ki.dwFlags = KEYEVENTF_KEYUP; 
+
     SendInput(1, &inp, sizeof(INPUT));
 
     for(auto i=0; i<counter; i++)
@@ -108,8 +114,37 @@ __declspec(dllexport) LRESULT CALLBACK KeyboardEvent (int nCode, WPARAM wParam, 
     return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
 }
 
+LPBYTE ReadRegValue(HKEY root, LPCSTR key, LPCSTR lpValueName, long unsigned int & lpcbData /* max length (bytes) */)
+{
+    HKEY hKey;
+    if (RegOpenKeyExA(root, key, 0, KEY_READ, &hKey) == ERROR_SUCCESS)
+    {
+        LPBYTE lpData = new BYTE[lpcbData];
+        if(RegQueryValueExA(hKey, lpValueName, NULL, NULL, lpData, &lpcbData) == ERROR_SUCCESS)
+        {
+            RegCloseKey(hKey);
+            return lpData;
+        }
+        RegCloseKey(hKey);
+    }
+    return nullptr;
+}
+
 int main(int argc, char** argv)
 {
+    hotkey = '1'; // Alt+Shift is default
+    {
+        long unsigned int tempSize = 4;
+        LPBYTE tempBuf = ReadRegValue(HKEY_CURRENT_USER, "Keyboard Layout\\Toggle", "Hotkey", tempSize);
+        if(tempBuf != nullptr) { hotkey = tempBuf[0]; delete tempBuf; }
+    }
+    if(hotkey == '2') // Control+Shift
+        key1 = 162;
+    else if(hotkey == '4') // [~`] key
+    {
+        key1 = 192; key2 = 0;
+    }
+
     hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)KeyboardEvent, NULL, 0);
     MSG message;
     while (GetMessage(&message,NULL,0,0) > 0) 
